8c30a089d6400bbd59da4428a65c4da87da996ea
Tanh <Anh1412sun@gmail.com>
2025-11-03 20:13:23 +0700

    feat: refactor application structure and add database module
    - Removed AppController and AppService as they are no longer needed.
    - Introduced DatabaseModule for database configuration using TypeORM with MySQL.
    - Added @nestjs/config and @nestjs/typeorm dependencies for configuration and database interaction.
    - Updated AppModule to include ConfigModule and DatabaseModule.

---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_DATABASE'),
+                autoLoadEntities: true, // Tự động load các file entity
+                synchronize: false, // Không tự động đồng bộ CSDL
+            }),
+        }),
+    ],
+    exports: [TypeOrmModule], // Xuất TypeOrmModule để các module khác có thể dùng forFeature
+})
+export class DatabaseModule { }
