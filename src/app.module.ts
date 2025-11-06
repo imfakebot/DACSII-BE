a6dcce46774679460aa54deb6a29dbf4e11003e4
Tanh <Anh1412sun@gmail.com>
2025-11-06 10:24:14 +0700

    feat: add modules for locations, fields, bookings, pricing, payments, vouchers, reviews, notifications, and feedbacks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,20 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { LocationModule } from './locations/locations.module';
+import { FieldsModule } from './fields/fields.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { PricingModule } from './pricing/pricing.module';
+import { PaymentsModule } from './payments/payments.module';
+import { VouchersModule } from './vouchers/vouchers.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { FeedbacksModule } from './feedbacks/feedbacks.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }), DatabaseModule, AuthModule, UsersModule],
+  }), DatabaseModule, AuthModule, UsersModule, LocationModule, FieldsModule, BookingsModule, PricingModule, PaymentsModule, VouchersModule, ReviewsModule, NotificationsModule, FeedbacksModule],
   controllers: [],
   providers: [],
 })
