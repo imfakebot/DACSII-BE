d025563a74770eaafceec8f14183df95d9590ff9
Tanh <Anh1412sun@gmail.com>
2025-11-06 08:17:22 +0700

    feat: add authentication and user management modules with tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }), DatabaseModule],
+  }), DatabaseModule, AuthModule, UsersModule,],
   controllers: [],
   providers: [],
 })
